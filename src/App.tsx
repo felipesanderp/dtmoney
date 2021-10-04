20035a652c65b212a7fb1b60a0e8904b56b91c63
felipesanderp <felipe.panisson@hotmail.com>
2021-10-04 10:48:39 -0300

    add react-modal to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+	const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+  function handleOpenNewTrasactionModal() {
+    setIsNewTrasactionModalOpen(true);
+  }
+
+  function handleCloseNewTrasactionModal() {
+    setIsNewTrasactionModalOpen(false);
+  }
+
 	return (
 		<>
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTrasactionModal} />
 			<Dashboard />
+
+			<Modal 
+         	isOpen={isNewTrasactionModalOpen} 
+          onRequestClose={handleCloseNewTrasactionModal}
+        >
+          <h2>Cadastro de transação</h2>
+        </Modal>
 			<GlobalStyle />
 		</>
 	);
