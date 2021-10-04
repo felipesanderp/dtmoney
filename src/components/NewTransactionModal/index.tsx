5de8a3ca2d6a465df370efa61123a24a0dd92167
felipesanderp <felipe.panisson@hotmail.com>
2021-10-04 16:37:58 -0300

    insert data in miragejs model

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,8 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
+import { api } from '../../services/api';
+
 import { Container, TransacitionTypeContainer, RadioBox } from './styles'
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -20,12 +22,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type
-    })
+    };
+
+    api.post('/transactions', data);
   }
   
   return (
