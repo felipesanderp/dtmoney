15e6440cfbeb97dd3295d13c681889500de86176
Felipe Sander <felipe.panisson@hotmail.com>
2022-03-22 16:56:21 -0300

    add signOut button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { NavLink, useLocation } from "react-router-dom"
 import logoImg from "../../assets/logo.svg"
+import { useAuth } from "../../hooks/useAuth";
 
 import { Container, Content } from './styles'
 
@@ -9,11 +10,13 @@ interface HeaderProps {
 
 export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   const { pathname } = useLocation();
+  const { signOut } = useAuth();
 
   return (
     <Container>
       <Content>
         <div className="header-items">
+          <button type="button" onClick={() => signOut()}>Sair</button>
           <img src={logoImg} alt="dt money" />
           <nav>
             <NavLink to="/dashboard">Listagem</NavLink>
