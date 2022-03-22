3f4494b8438fb4a8a3aa1faf0fa6d6cbc81b43e6
Felipe Sander <felipe.panisson@hotmail.com>
2022-03-22 11:49:35 -0300

    add form to signIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,18 @@
+import { Container, Content } from './styles';
+
 export function SignIn() {
   return (
-    <h1>SigIn page</h1>
+    <Container>
+      <Content>
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input type="email" name="email" placeholder="E-mail" />
+          <input type="password" name="Password" placeholder="Password" />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </Content>
+    </Container>
   )
 }
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1120px;
+
+  display: flex;
+  justify-content: center;
+
+  form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    width: 100%;
+    max-width: 360px;
+
+    h1 {
+      color: var(--text-title);
+      font-size: 2rem;
+      margin-bottom: 2rem;
+    }
+
+    input {
+      width: 100%;
+      padding: 0 1.5rem;
+      height: 4rem;
+      border-radius: 0.25rem;
+
+      border: 1px solid #d7d7d7;
+      background-color: #e7e9ee;
+
+      font-weight: 400;
+      font-size: 1rem;
+
+      &::placeholder {
+        color: var(--text-body);
+      }
+
+      & + input {
+        margin-top: 1rem;
+      }
+    }
+
+    button[type="submit"] {
+      width: 100%;
+      padding: 0 1.5rem;
+      height: 4rem;
+      background: var(--green);
+      color: #fff;
+      border-radius: 0.25rem;
+      border: 0;
+      font-size: 1rem;
+      font-weight: 600;
+      margin-top: 1.5rem;
+
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.9);
+      }
+    }
+  }
+`;
