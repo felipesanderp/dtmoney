e1a7a5058351a73b13abc4f5fe54b70c4f5f83f1
Felipe Sander <felipe.panisson@hotmail.com>
2022-03-22 16:57:56 -0300

    make some routes private

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,34 @@
-import { Routes as RoutesReact, Route } from 'react-router-dom';
+import { Routes as RoutesReact, Route, useLocation, Navigate, Outlet } from 'react-router-dom';
 
 import { SignIn } from '../pages/SignIn';
 import { Dashboard } from '../pages/Dashboard';
 import { Import } from '../pages/Import';
+import { useAuth } from '../hooks/useAuth';
 
 export function Routes() {
+ 
   return (
     <RoutesReact>
       <Route path="/" element={<SignIn />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/import" element={<Import />} />
+      <Route element={<RequireAuth />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/import" element={<Import />} />
+      </Route>
     </RoutesReact>
   )
+}
+
+function RequireAuth() {
+  const { user } = useAuth();
+  let location = useLocation();
+
+  if (!user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/" state={{ from: location }} />;
+  }
+
+  return <Outlet />;
 }
